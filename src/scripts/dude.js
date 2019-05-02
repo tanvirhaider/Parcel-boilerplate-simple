2fbdd3738906228f4afada7261dc539e8e565f9f
tanvir haider <tanvir@willow.studio>
2019-05-02 10:21:23 -0400

    new

--- src/scripts/dude.js
+++ src/scripts/dude.js
@@ -1,7 +1,7 @@
 
 
-const dude = (greeting) => {
-    return `${greeting}, dude`;
+const dude = (heydude) => {
+    return `${heydude}, where is my car?`;
 }
 
 export { dude }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,10 @@
 
-import { dude } from './dude'
-import ScrollMagic from 'scrollmagic';
-
-import '../styles/main.scss'
-import { TweenMax } from 'gsap';
 
 
-document.querySelector('h1').textContent = dude(`how's it going`);
+import '../styles/main.scss'                // importing scss file
+import { dude } from './dude'               // importing a test javascript module
+import { TweenMax } from 'gsap';            // importing gsap animation engine 
+import ScrollMagic from 'scrollmagic';      // importing scroll magic for parallax functionality 
+
+
+document.querySelector('h1').textContent = dude(`hey Joffrey`);
