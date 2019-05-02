5a6924029cd62a9b55b8334a6b135152424a2c3b
tanvir haider <tanvir@willow.studio>
2019-05-02 00:02:23 -0400

    first init

---
+++ src/scripts/dude.js
@@ -0,0 +1,7 @@
+
+
+const dude = (greeting) => {
+    return `${greeting}, dude`;
+}
+
+export { dude }
---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+
+import { dude } from './dude'
+import ScrollMagic from 'scrollmagic';
+
+import '../styles/main.scss'
+import { TweenMax } from 'gsap';
+
+
+document.querySelector('h1').textContent = dude(`how's it going`);
