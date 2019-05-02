4d904a437247b10fc9132f202357a85ee997e5fd
tanvir haider <tanvir@willow.studio>
2019-05-02 00:13:33 -0400

    new

--- dist/scripts.bcf3243b.js
+++ dist/scripts.bcf3243b.js
@@ -14183,7 +14183,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64302" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64414" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
